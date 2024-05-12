19eb57ba6c22868ee1b382ca2340372e342db275
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 11:25:27 +0300

    Added design for Cell.

---
+++ MineSweeper/Forms/Cell.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MineSweeper.Forms
+{
+    public partial class Cell : UserControl
+    {
+       
+    }
+}

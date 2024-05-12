01023c7e59144e2f2437375a4dcee1fa679340bd
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 11:32:13 +0300

    Added Helpers Class and GameField Form design.

---
+++ MineSweeper/Classes/Helpers.cs
@@ -0,0 +1,12 @@
+﻿namespace MineSweeper.Classes
+{
+    public static class Helpers
+    {
+        public enum DifficultyLevel
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+    }
+}
---
+++ MineSweeper/Forms/GameField.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for GameField.xaml
+    /// </summary>
+    public partial class GameField : Window
+    {
+
+
+    }
+}

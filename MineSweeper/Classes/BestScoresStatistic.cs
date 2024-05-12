1ce2b5b416d5fd22d6d8b10cb4099078bf68e4db
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 14:00:50 +0300

    Added logic for best scores.

--- MineSweeper/Classes/BestScoresStatistic.cs
+++ MineSweeper/Classes/BestScoresStatistic.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 namespace MineSweeper.Classes
 {
     internal class BestScoresStatistic
@@ -29,6 +30,51 @@ namespace MineSweeper.Classes
                     Difficulty = parts[2]
                 });
             }
+
+        }
+
+        public string GetBestScores(string Difficulty)
+        {
+            BestScores = BestScores.OrderBy(x => x.Time).ToList();
+
+            var sb = new StringBuilder();
+            foreach (var score in BestScores.Where(x => x.Difficulty == Difficulty))
+                sb.AppendLine($"{score.Name} {score.Time} {score.Difficulty}");
+
+            return sb.ToString();
+        }
+
+        private bool CheckBestScore(string Difficulty, int Time)
+        {
+            var bestScore = BestScores.Where(x => x.Difficulty == Difficulty);
+            if (bestScore.Count() < 10)
+                return true;
+
+            foreach (var score in bestScore)
+                if (score.Time > Time)
+                    return true;
+
+            return false;
+        }
+
+        private void SaveBestScore(string Name, int Time, string Difficulty)
+        {
+            if (CheckBestScore(Difficulty, Time))
+            {
+                BestScores.Add(new BestScoreItem
+                {
+                    Name = Name,
+                    Time = Time,
+                    Difficulty = Difficulty
+                });
+
+                BestScores = BestScores.OrderBy(x => x.Time).ToList();
+                for (int i = 0; i < BestScores.Count; i++)
+                    if (BestScores[i].Difficulty == Difficulty)
+                        BestScores.RemoveAt(i);
+            }
+
+            File.WriteAllText(FILE_NAME, string.Join("\n", BestScores.Select(x => $"{x.Name} {x.Time} {x.Difficulty}")));
         }
     }
 }

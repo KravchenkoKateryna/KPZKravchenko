3c170b25457ae793c069a320fd35119e7a0f28c8
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 13:48:28 +0300

    Added Best score class and WinGame form.

---
+++ MineSweeper/Classes/BestScoresStatistic.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+namespace MineSweeper.Classes
+{
+    internal class BestScoresStatistic
+    {
+        const string FILE_NAME = "bestScores.txt";
+
+        private List<BestScoreItem> BestScores { get; set; }
+
+        public BestScoresStatistic()
+        {
+            BestScores = new List<BestScoreItem>();
+            LoadBestScores();
+        }
+
+        private void LoadBestScores()
+        {
+            if (!File.Exists(FILE_NAME))
+                return;
+
+            var lines = File.ReadAllLines(FILE_NAME);
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                BestScores.Add(new BestScoreItem
+                {
+                    Name = parts[0],
+                    Time = int.Parse(parts[1]),
+                    Difficulty = parts[2]
+                });
+            }
+        }
+    }
+}
---
+++ MineSweeper/Forms/WinGameForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MineSweeper.Forms
+{
+    /// <summary>
+    /// Interaction logic for WinGameForm.xaml
+    /// </summary>
+    public partial class WinGameForm : Window
+    {
+        public WinGameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

9bb098d5bebf8648fed3ccc5234eaf161e070cd3
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 11:17:09 +0300

    Added classes for Medium and Hard levels.

---
+++ MineSweeper/Classes/Levels/MediumLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace MineSweeper.Classes.Levels
+{
+    internal class MediumLevel : ILevel
+    {
+        public string Name { get; } = "Medium";
+        public int Width { get; } = 16;
+        public int Height { get; } = 16;
+        public int Bombs { get; } = 40;
+    }
+}

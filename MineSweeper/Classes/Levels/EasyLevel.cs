20eb4c4312c57bddde08897909812730fa4acb5c
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 11:13:37 +0300

    Added class for easy level.

---
+++ MineSweeper/Classes/Levels/EasyLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace MineSweeper.Classes.Levels
+{
+    internal class EasyLevel : ILevel
+    {
+        public string Name { get; } = "Easy";
+        public int Width { get; } = 10;
+        public int Height { get; } = 10;
+        public int Bombs { get; } = 10;
+    }
+}

8da9598a86a8b334ab24a786a28e3d6ad93d2570
KravchenkoKateryna <157889393+KravchenkoKateryna@users.noreply.github.com>
2024-05-12 11:12:31 +0300

    Added interface for difficulty level classes.

---
+++ MineSweeper/Classes/Levels/ILevel.cs
@@ -0,0 +1,10 @@
+﻿namespace MineSweeper.Classes.Levels
+{
+    public interface ILevel
+    {
+        public string Name { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public int Bombs { get; }
+    }
+}
